7850c21b0c0ac7106c6d2be200f2631a8151ab67
DeVelox <matanovic.antun@gmail.com>
2021-04-23 11:19:56 +0200

    Initial commit.

---
+++ scripts/talespire-dice.js
@@ -0,0 +1,5 @@
+Hooks.on("createChatMessage", (data) => {
+  data._roll ? window.open("talespire://dice/" + encodeURI(data.data.flavor) + ":" +
+    data._roll._formula.replace(/ /g, "")) :
+    console.log("No dice roll found.")
+})
