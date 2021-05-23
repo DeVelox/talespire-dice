5bb9033905bff2ebc39b9ad7f62577b6084bdbb1
DeVelox <matanovic.antun@gmail.com>
2021-05-23 14:17:12 +0200

    Added compatibility with Foundry 0.8.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Patch Notes:
 
+## Version 0.1.5
+- Added compatibility with Foundry 0.8.5
+
 ## Version 0.1.1
 - Added a warning when BetterRolls5e is enabled
 - Check there is a dice roll before sending to Talespire
--- scripts/talespire-dice.js
+++ scripts/talespire-dice.js
@@ -1,26 +1,8 @@
 Hooks.once("ready", () => {
+  console.log("talespire-dice | Initializing talespire-dice");
   if (!(game.modules.has("betterrolls5e") && game.modules.get("betterrolls5e").active)) {
     Hooks.on("preCreateChatMessage", (msg) => {
-      if (msg.roll && game.settings.get("talespire-dice", "rollFoundry") !== 1) {
-        const flavor = msg.flavor ? parseFlavorText(msg.flavor) : "dice";
-        const formula = parseRollFormula(JSON.parse(msg.roll).formula);
-        if (formula == "crit") {
-          ui.notifications.error("Talespire currently doesn't support multiplication to calculate critical hits. Please roll damage normally then double it.");
-          return false;
-        }
-        if (formula == "nodice") {
-          console.log("No dice roll found.");
-        }
-        else {
-          window.open("talespire://dice/" + flavor + ":" + formula);
-          if (game.settings.get("talespire-dice", "rollFoundry") == 0) {
-            return false;
-          }
-        }
-      }
-      else {
-        console.log("No dice roll found.");
-      }
+      processRolls(msg);
     });
   }
   else {
@@ -72,4 +54,44 @@ function addMods(formula) {
   const dice = Array.from(formula.matchAll(/(\d*d\d+)/g), i => i[0]);
   const mods = Array.from(formula.matchAll(/([+-]\d+)(?!d)/g), i => i[0]).reduce((a, b) => a + parseInt(b), 0);
   return dice.join("+") + (mods >= 0 ? "+" : "") + mods;
+}
+
+async function processRolls(msg) {
+  let flavor;
+  let formula;
+  let isRoll;
+
+  if (parseFloat(game.data.version) >= 0.8) {
+    if (msg.isRoll) {
+      flavor = msg.roll.options.flavor ? parseFlavorText(msg.roll.options.flavor) : "dice";
+      formula = parseRollFormula(msg.roll.formula);
+      isRoll = msg.isRoll;
+    }
+  }
+  else {
+    if (msg.roll) {
+      flavor = msg.flavor ? parseFlavorText(msg.flavor) : "dice";
+      formula = parseRollFormula(JSON.parse(msg.roll).formula);
+      isRoll = msg.roll ? true : false;
+    }
+  }
+
+  if (isRoll && game.settings.get("talespire-dice", "rollFoundry") !== 1) {
+    if (formula == "crit") {
+      ui.notifications.error("Talespire currently doesn't support multiplication to calculate critical hits. Please roll damage normally then double it.");
+      return false;
+    }
+    if (formula == "nodice") {
+      console.log("talespire-dice | No dice roll found.");
+    }
+    else {
+      window.open("talespire://dice/" + flavor + ":" + formula);
+      if (game.settings.get("talespire-dice", "rollFoundry") == 0) {
+        return false;
+      }
+    }
+  }
+  else {
+    console.log("talespire-dice | No dice roll found.");
+  }
 }
